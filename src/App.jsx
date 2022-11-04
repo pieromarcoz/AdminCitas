6a19856d0d50b8ef1f6c2a1cbd546a925396f18c
piero965d <piero965d@gmail.com>
2022-11-04 02:56:13 -0500

    'UseEffect'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [pacientes, setPacientes] = useState([])
-  const [paciente, setPaciente] = useState([])
+
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? [])
+  const [paciente, setPaciente] = useState({})
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes])
+
   const eliminarPaciente = id => {
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(pacientesActualizados)
